50d7ec94cd213c946a4833a27c32e0393401615a
arfanur04 <arfanur04@gmail.com>
2025-09-01 14:24:06 +0600

    Add to call history feature added

--- js/index.js
+++ js/index.js
@@ -4,3 +4,41 @@ document.querySelectorAll(".heart-btn").forEach((heartBtn) => {
 		heartCount.innerText = +heartCount.innerText + 1;
 	});
 });
+
+document.querySelectorAll(".call-btn").forEach((callBtn) => {
+	callBtn.addEventListener("click", (e) => {
+		const cardShortNameId = e.target.parentElement.id;
+
+		const cardFullName = document.getElementById(
+			`${cardShortNameId}-title`
+		)?.innerText;
+
+		const cardNum = document.getElementById(
+			`${cardShortNameId}-number`
+		)?.innerText;
+
+		const coinCountElem = document.getElementById("coin-count");
+		if (cardFullName && cardNum && +coinCountElem.innerText >= 20) {
+			//: Show alert
+			const callAlert = `📞 ${cardFullName} ${cardNum} ...`;
+			// alert(callAlert);
+			coinCountElem.innerText = +coinCountElem.innerText - 20;
+			const callHistoryContainer = document.getElementById(
+				"call-history-container"
+			);
+			const callHistoryItem = document.createElement("div");
+			callHistoryItem.innerHTML = `
+            <div class="flex justify-between items-center mb-4">
+               <div class="w-2/3">
+                  <h5 class="font-bold">${cardFullName}</h5>
+                  <p>${cardNum}</p>
+               </div>
+               <p>${new Date().toLocaleTimeString()}</p>
+            </div>
+         `;
+			callHistoryContainer.appendChild(callHistoryItem);
+		} else if (+coinCountElem.innerText < 20) {
+			alert("You don't have enough coins");
+		}
+	});
+});
